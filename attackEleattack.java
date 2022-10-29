4b0df962326abc32f2909a7f09afcc7f12f83708
Zhaoyang(Ray) Wang <Mahuahua@users.noreply.github.com>
2022-10-29 01:10:18 -0400

    Add files via upload

---
+++ attackEleattack.java
@@ -0,0 +1,49 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        int userInput = 500;//change when you want
+        double[][][] a = new double[6][6][3000]; //a[x1,x2,y] here is after x1 attack and x2 element attack rolls, the count of landing on each element attack value
+        a[0][0][52+69] = 1;//assuming the gear has both attack and element before rolling
+        
+        for (int i1=0;i1<=5;i1++){
+        for (int i2=0;i2<=5-i1;i2++){
+            for (int j=0;j<2000;j++){
+                if (i1-1 >=0){
+                for (int k=93;k<=234;k++){
+                    a[i1][i2][j+k]=a[i1][i2][j+k]+a[i1-1][i2][j];
+                }
+                }
+                if (i2-1 >=0){
+                for (int k=125;k<=312;k++){
+                    a[i1][i2][j+k]=a[i1][i2][j+k]+a[i1][i2-1][j];
+                }
+                }
+            }
+        }
+        }
+        double[][] total = new double[6][6];
+        double[][] aboveUser = new double[6][6];
+     
+        for (int i1=0;i1<=5;i1++){
+        for (int i2=0;i2<=5-i1;i2++){
+         total[i1][i2] =0;
+         aboveUser[i1][i2] =0;
+         for (int j=0;j<2000;j++){
+             total[i1][i2] = total[i1][i2] + a[i1][i2][j];
+             if (j>=userInput){
+                 aboveUser[i1][i2] = aboveUser[i1][i2] + a[i1][i2][j];
+             }
+         }
+        }
+        }
+        
+        for (int i1=0;i1<=5;i1++){
+        for (int i2=0;i2<=5-i1;i2++){
+         System.out.println("Chance for crit above " + userInput + " after rolling attack " + i1 +" times and rolling element attack "+ i2+ " times is " + aboveUser[i1][i2]/total[i1][i2]);
+        }
+        }
+        
+    }
+}
---
+++ critRoll.java
@@ -0,0 +1,34 @@
+class findCritRolls{
+    public static void main(String[] args) {
+        int userInput = 3600;//change when you want
+        double[][] a = new double[6][8000]; //a[x,y] here is after x crit rolls, the count of landing on each crit
+        a[0][258] = 1;
+        for (int i=1; i<=5; i++){
+            for (int j=0;j<6105;j++){
+                for (int k=468;k<=1169;k++){
+                    a[i][j+k]=a[i][j+k]+a[i-1][j];
+                }
+            }
+        }
+        double[] total = new double[6];
+        double[] aboveUser = new double[6];
+        for (int i=0;i<=5;i++){
+         total[i] =0;
+         aboveUser[i] =0;
+         for (int j=0;j<6105;j++){
+             total[i] = total[i] + a[i][j];
+             if (j>=userInput){
+                 aboveUser[i] = aboveUser[i] + a[i][j];
+             }
+         }
+        }
+        double[] chanceOfXPulls = {0.24,0.396,0.264,0.088,0.0150, 0.001};
+        double total_chance = 0;
+        for (int i=0;i<=5;i++){
+         System.out.println("Chance for crit above " + userInput + " after rolling crit " + i +" times is " + aboveUser[i]/total[i]);
+            total_chance = total_chance + chanceOfXPulls[i]*aboveUser[i]/total[i];
+        }
+        
+        System.out.println("Total chance is " + total_chance);
+    }
+}
